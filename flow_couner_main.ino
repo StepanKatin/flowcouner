724380f5f75337c4600af501c6b5b0b754c5e122
Stepan Katin <katin_stepan@mail.ru>
2024-10-15 01:50:12 +0300

    написал код запуска насоса и счет с СБМ
    Думаю стоит сделать отдельную ветку для обработки сигнала с помощью прерывания (перезаписывая переменную countSBM на ноль при завершении прокачки жидкости до счетчика), а в основной ветке оставить версию с обработкой понижения напряжения на аналоговом пине эксперементально определив порог понижения напряжения для повышения счетчика на 1.

--- flow_couner_main.ino
+++ flow_couner_main.ino
@@ -19,12 +19,16 @@
 
 // ОБЪЯВЛЯЕМ ВНУТРЕННИЕ ПЕРЕМЕННЫЕ И КОНСТАНТЫ
 
-#define SKVASHN = 500 // скважность, треубется подобрать что бы на СБМ поступало 400В
+#define SKVASHN 500 // скважность, треубется подобрать что бы на СБМ поступало 400В
 
 volatile unsigned long counts_from_SBM = 0;  // счетчик импульсов
 bool isRecording = false;  // Флаг записи показаний
+unsigned long long initialtime = millis();
 unsigned long long startTime = 0; // Время начала записи
+unsigned long long pumptime_start = 0; // Время начала прокачки мотора
+const int pumping_time = 60000; // Время работы мотора
 const int recordingDuration = 60000; // Длительность записи (60 секунд), устанавливается в зависимости от скорости мотора и диаметра тру
+
 int value_pwm = 0;
 const int max_pwm = 255;
 boolean event_perticle = LOW;
@@ -48,33 +52,65 @@ void setup() {
   pinMode(PWM_SBM, OUTPUT);
   PWM_16KHZ_D9(SKVASHN);  // ШИМ 16 кГц на пине D9, заполнение SKVASHN из 1023
   pinMode(SBM_21, INPUT_PULLUP); // подключили катод датчика на A2
-  attach
+  
 
 
   TCCR1A = 0b00000001;  // 8bit
   TCCR1B = 0b00001001;  // x1 fast pwm
 
-  attachInterrupt(0, btnIsr, FALLING);
+  attachInterrupt(2, btnIsr, FALLING);
 
+}
+
+void btnIsr() {
+  counts_from_SBM ++;
+   
 }
 
 void loop() {
 	// обязательная функция отработки. Должна постоянно опрашиваться
   enc.tick();
-  
-  
+   
 
   if (enc.isTurn()) { // если был совершён поворот (индикатор поворота в любую сторону)
       if (enc.isRight() && value_pwm < max_pwm) value_pwm += 5;
       if (enc.isLeft()&& value_pwm > 0) value_pwm -=5;
       lcd.setCursor(10, 0);
-      lcd.print(value);  // выводим значение при повороте
+      lcd.print(value_pwm);  // выводим значение при повороте
   }
   if (enc.isClick()) {
+    pumptime_start = millis();
+      if ((pumptime_start - initialtime) < pumping_time) {
       analogWrite(PWM_MOTOR, value_pwm);
   
-      digitalWrite(DIR_MOTOR, HIGH); 
+      digitalWrite(DIR_MOTOR, HIGH);
+      }
+      if ((pumptime_start - initialtime) > pumping_time) {
+      analogWrite(PWM_MOTOR, 0);
+      isRecording = true; 
+
+      }
+    initialtime = pumptime_start;    
+  } if (isRecording = true) {
+    isRecording = false;
+    counts_from_SBM = 0;
+    startTime = millis();
+    if ((startTime - initialtime) < recordingDuration){
+      counts_from_SBM ++;
+      analogWrite(PWM_MOTOR, value_pwm);
+      digitalWrite(DIR_MOTOR, HIGH);
+    }
+    if ((startTime - initialtime) > recordingDuration){
+      lcd.setCursor(10, 1);
+      lcd.print(counts_from_SBM);
+      counts_from_SBM = 0;
+      analogWrite(PWM_MOTOR, value_pwm);
+
+    }
 
   }
+}  
+
+  
  
 
